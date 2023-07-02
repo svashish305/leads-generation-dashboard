d9fb12841d102dbec5debb287ec28255ff0778fc
Shubhashish Verma <svashish305@gmail.com>
2023-07-02 12:21:24 +0530

    added rate limiter on server

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 import connectDB from "./config/db.js";
 import routes from "./routes/index.js";
 
@@ -8,6 +9,11 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(rateLimit({
+  windowMs: process.env.RATELIMIT_WINDOW,
+  max: process.env.RATELIMIT_THRESHOLD,
+  message: `Too many requests from this IP, please try again after ${process.env.RATELIMIT_WINDOW_STRING}`,
+}));
 
 connectDB();
 
